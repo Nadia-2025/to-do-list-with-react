8fc485bbf09d201e3e6e9c5b50d1e58aeb4c8c1a
Nadia-2025 <nadyah-88@hotmail.com>
2025-05-27 18:03:06 +0200

    feat(ui): build base UI components and css styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,32 @@
 
 
 function App() {
-   return (
-    <h1>holaaa</h1>
-   )
-    
+  return (
+  <div className="container">
+    <div className="heading">
+      <h1>To-Do List</h1>
+    </div>
+    <div className="form">
+      <input type="text"></input>
+      <button>
+        <span>Add</span>
+      </button>
+    </div>
+    <div>
+      <ul className="">
+        <li >
+
+        </li>
+         <li >
+
+        </li>
+      </ul>
+    </div>
+      
+
+  </div>
+  )
+
 }
 
 export default App;
