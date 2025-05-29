1035b6ce42c5050b99c0c556f6dd3ae3dc6d9d45
Nadia-2025 <nadyah-88@hotmail.com>
2025-05-29 09:45:58 +0200

    feat:Add css style to the <li> items

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -8,14 +8,20 @@ function handleOnClick() {
 }
 
     return (
-        <li onClick={handleOnClick}
-        style={{textDecoration: IsDone ? "line-through" : "none",
-            cursor:"pointer"
-        }}>{props.text}
-        <span>
+      <li> 
+        <span
+          className="itemText" 
+          onClick={handleOnClick}
+          style={{textDecoration: IsDone ? "line-through" : "none",
+          cursor:"pointer"
+          }}
+        >{props.text}
+        </span>
+        <span
+        className="deadlineText">
           (Due: {props.deadline})
         </span>
-        </li>
+      </li>
     )
 }
 
